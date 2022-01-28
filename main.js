1fe6bc46292479cfecb3e2942c86d49892a8ade5
kirin Thapar <62936453+kirint93@users.noreply.github.com>
2022-01-28 10:09:01 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+myFunction =() => {
+ const checkBox = document.getElementsByClassName("no");
+ const textBox = document.getElementsByClassName("textArea");
+ for ( i=0;i<checkBox.length; i++) {
+    if (checkBox[i].checked == true){
+    textBox[i].style.display = "block";
+  } else {
+    textBox[i].style.display = "none";
+  }
+}
+  }
+
+ 
